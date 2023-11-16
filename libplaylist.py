1a3aac5a0c6bbde125888fc25fa0dde2ac9ee798
Juha-Matti Tilli <juha-matti.tilli@iki.fi>
2023-11-16 19:19:23 +0200

    Add support for PLS playlists

--- libplaylist.py
+++ libplaylist.py
@@ -7,6 +7,53 @@ import subprocess
 
 MAX_LINE = 4097
 
+def get_pls_playlist(x):
+    tag_seen = False
+    entries = {}
+    number_of_entries = None
+    with open(x, "r") as f:
+        while True:
+            l = f.readline(MAX_LINE)
+            if len(l) == 0:
+                res = []
+                if number_of_entries < 1:
+                    return None
+                for k in range(1, number_of_entries+1):
+                    if k not in entries:
+                        return None
+                    res.append(entries[k])
+                return res
+            if len(l) == MAX_LINE:
+                return None
+            if l[0] == ';':
+                continue
+            if re.match("^[ \t]*;", l):
+                continue
+            if l[-1] == '\n':
+                l = l[:-1]
+            if re.match("^[ \t]*$", l):
+                continue
+            if l == '[playlist]':
+                tag_seen = True
+                continue
+            if not tag_seen:
+                return None
+            if l[0] == '[':
+                return None
+            if l[:4] == "File" and "=" in l:
+                key,val = l.split("=", 1)
+                try:
+                    idx = int(key[4:])
+                    entries[idx] = val
+                except:
+                    return None
+            if l[:16] == "NumberOfEntries=":
+                try:
+                    number_of_entries = int(l[16:])
+                except:
+                    return None
+
+
 def get_m3u_playlist(x):
     first_line = True
     is_ext = False
@@ -108,7 +155,10 @@ def get_m3u_playlist(x):
                         res.append(key)
 
 def get_playlist(x):
-    m3u = get_m3u_playlist(x)
-    if m3u is not None:
-        return m3u
+    y = get_m3u_playlist(x)
+    if y is not None:
+        return y
+    y = get_pls_playlist(x)
+    if y is not None:
+        return y
     return None
